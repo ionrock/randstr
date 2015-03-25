e2998a62f529d89fd57a8a321b0a38640d89c640
Eric Larson <eric@ionrock.org>
2015-03-25 18:02:25 -0500

    Add some predefined list of characters folks might want to use.
    This adds:
    
     - ALPHA
     - NUMS
     - DELIMITERS
     - CHARS_WITH_CAPS
     - CHARS_WITH_DELIMITERS
    
    That should be good enough to compose whatever sort of string you want.

--- randstr/__init__.py
+++ randstr/__init__.py
@@ -5,8 +5,12 @@ __author__ = 'Eric Larson'
 __email__ = 'eric@ionrock.org'
 __version__ = '0.1.1'
 
-
-CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
+ALPHA = 'abcdefghijklmnopqrstuvwxyz'
+NUMS = '0123456789'
+DELIMITERS = ' -_.,'
+CHARS = ALPHA + NUMS
+CHARS_WITH_CAPS = ALPHA.upper() + CHARS
+CHARS_WITH_DELIMITERS = CHARS + DELIMITERS
 
 
 def randstr(length=7, chars=CHARS):
